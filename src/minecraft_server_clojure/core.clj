7a1a9ee9ac53699fce36ab805f1ff1560a881a3f
p1k0chu <anatoliyid0207@gmail.com>
2025-07-27 16:05:55 +0300

    :3 server

--- src/minecraft_server_clojure/core.clj
+++ src/minecraft_server_clojure/core.clj
@@ -1,5 +1,16 @@
-(ns minecraft-server-clojure.core)
+(ns minecraft-server-clojure.core
+  (:import (java.io PrintWriter)
+           (java.net ServerSocket)))
 
 (defn -main [& args]
   "the main"
-  (println "Hello world"))
+  (let [socket (ServerSocket. 25565)]
+    (while true
+      (let [client-sock (.accept socket)]
+        (do
+          (let [writer (PrintWriter.
+                         (.getOutputStream
+                           client-sock)
+                         true)]
+            (.println writer ":3"))
+          (.close client-sock))))))
